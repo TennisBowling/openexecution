8a4e7c64814def25f9ca80b08b239b2d87261867
TennisBowling <tennisbowling@tennisbowling.com>
2022-08-08 18:11:21 -0700

    remove old submodules and add new

--- main.cpp
+++ main.cpp
@@ -2,30 +2,18 @@
 #include <spdlog/spdlog.h>
 #include <nlohmann/json.hpp>
 #include "util.hpp"
-#include "Simple-Web-Server/server_http.hpp"
 #include <boost/asio/thread_pool.hpp>
 #include <boost/asio/post.hpp>
 #include <string>
 #include <iostream>
-#include <pqxx/pqxx>
 
 int main(int argc, char *argv[])
 {
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] - %v"); // nice style that i like
 
-    // setup postgres
-    try
-    {
-        pqxx::connection db("postgresql://postgres:postgres@localhost:5432/postgres");
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    // setup sqlite
+    auto db = sqlitelib::Sqlite("db.sqlite");
+    db.execute("CREATE TABLE IF NOT EXISTS fcu_data (VALUE TEXT);");
 
-    // pqxx::work txn(db);
-    // txn.exec("CREATE TABLE IF NOT EXISTS fcu_data (VALUE TEXT);");
-    // txn.commit();
-
-    spdlog::info("Postgresql connection established");
+    spdlog::info("Sqlite connection established");
 }
